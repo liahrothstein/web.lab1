dcd2b4e752c3cbdb5369e9aee815d37d94f9aa2f
liahrothstein <liahrothstein@gmail.com>
2026-02-11 20:56:00 +0300

    feat: done main page

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,10 +1,44 @@
+import { Link } from 'react-router-dom';
+
 import './MainPage.scss';
 
 export function MainPage() {
 
-  return (
-    <div className="mainPage">
-        
-    </div>
-  )
+    return (
+        <div className="mainPage">
+            <main>
+                <h1>Главная страница</h1>
+                <nav>
+                    <ul>
+                        <li><Link to='/'>Обо мне</Link></li>
+                        <li><Link to='/'>Мои хобби</Link></li>
+                        <li><Link to='/'>Фотоальбом</Link></li>
+                        <li><Link to='/'>Стажировка</Link></li>
+                    </ul>
+                </nav>
+            </main>
+            <div className="site-map-container">
+                <h2>Архитектура проекта</h2>
+                <div className="map-visual">
+                    <div className="map-node root">
+                        <span>Главная страница</span>
+                        <div className="map-branches">
+                            <div className="branch">
+                                <div className="node">Обо мне</div>
+                            </div>
+                            <div className="branch">
+                                <div className="node">Мои хобби</div>
+                            </div>
+                            <div className="branch">
+                                <div className="node">Фотоальбом</div>
+                            </div>
+                            <div className="branch">
+                                <div className="node">Стажировка</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
