a93c818ae981b555c35ebfd9310a5f91e16447d8
liahrothstein <liahrothstein@gmail.com>
2026-02-11 20:00:39 +0300

    feat: complete index.html

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,16 @@ import './App.scss'
 export function App() {
 
   return (
-    <div className="app"></div>
+    <div className="index">
+      <main>
+        <h1>Стартовая страница</h1>
+        <ul>
+          <li>Название лабораторной работы: </li><p>Структура HTML документов №1</p>
+          <li>Цель: </li><p>Изучение структуры и реализация HTML документов</p>
+          <li>ФИО: </li><p>Новиков Дмитрий Алексеевич</p>
+          <li>Группа: </li><p>КФ-17</p>
+        </ul>
+      </main>
+    </div>
   )
 }
